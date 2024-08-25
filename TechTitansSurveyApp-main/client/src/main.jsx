fdc2a4f8d0e6688d34ee3f81cce91e40bcb794fa
Noor Khamees <115987719+Noorulhuda87@users.noreply.github.com>
2024-08-25 17:48:32 -0400

    Update main.jsx

--- TechTitansSurveyApp-main/client/src/main.jsx
+++ TechTitansSurveyApp-main/client/src/main.jsx
@@ -1,13 +1,7 @@
 /*
-    Project: Tech Titans Survey App
-    Class: Comp229 Sec 402
-    Authors: 
-        Noorulhuda Khamees  – 301291589
-        Ronald Hardock      – 301274360
-        Amer Ajjawi         – 301319092
-        Blake Hewitt        – 301279469
-        Gabriel Normand     – 301293488
-        Jordi Llobet Ferre  – 301261208
+    
+    Authors: Noorulhuda Khamees  – 301291589
+
 
     File Description:
     This file defines the entry point where the component App containing all other components is rendered for the Survey App.
